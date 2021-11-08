750e4ca1a03359f5e710993fe59b88cca3d16bc6
Rume7 <electengrhume@gmail.com>
2021-11-08 03:39:18 +0100

    Added the calculate January expense method

--- src/main/java/com/codehacks/chapter1/BankTransactionAnalyzerSimple.java
+++ src/main/java/com/codehacks/chapter1/BankTransactionAnalyzerSimple.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 /**
@@ -12,11 +15,11 @@ import java.util.List;
  */
 public class BankTransactionAnalyzerSimple {
 
-    private static final String RESOURCES = "src/main/resources/";
+    private static final String RESOURCES = "src/main/java/statement.txt";
 
     public static void main(final String... args) throws IOException {
 
-        final Path path = Paths.get(RESOURCES + args[0]);
+        final Path path = Paths.get(RESOURCES);
         final List<String> lines = Files.readAllLines(path);
         double total = 0d;
         for (final String line : lines) {
@@ -26,6 +29,22 @@ public class BankTransactionAnalyzerSimple {
         }
 
         System.out.println("The total for all transactions is " + total);
+    }
+
+    public void calculateJanuaryExpense() throws IOException {
+        final Path path = Paths.get(RESOURCES);
+        final List<String> lines = Files.readAllLines(path);
+        double total = 0d;
+        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        for (final String line : lines) {
+            final String[] columns = line.split(",");
+            final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+            if (date.getMonth() == Month.JANUARY) {
+                final double amount = Double.parseDouble(columns[1]);
+                total += amount;
+            }
+        }
+        System.out.println("The total for all transactions in January is " + total);
     }
 
 }
